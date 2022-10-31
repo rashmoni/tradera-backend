847accd69e5de4152549c79ce98609b2700c9fb2
rashmoni <rashudey9851@gmail.com>
2022-10-31 21:17:40 +0100

    initial commit

---
+++ src/main/java/com/novare/traderabackend/TraderaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.novare.traderabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraderaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraderaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/novare/traderabackend/TraderaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.novare.traderabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraderaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
