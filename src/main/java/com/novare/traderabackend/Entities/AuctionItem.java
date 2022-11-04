9b4b7171897c027e1cbf5f50072ee69066d54c1e
Malla <mallagronqvist@gmail.com>
2022-11-04 16:25:27 +0100

    Added some constraint validation. Refactored Date to Timestamp for compatibility with mySQL timestamp.

--- src/main/java/com/novare/traderabackend/Entities/AuctionItem.java
+++ src/main/java/com/novare/traderabackend/Entities/AuctionItem.java
@@ -4,6 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.sql.Timestamp;
 import java.util.Date;
 
 @Entity
@@ -16,14 +19,20 @@ public class AuctionItem {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
+    @NotBlank(message = "Name is required.")
     private String item_name;
+    @NotBlank(message= "Description is required.")
     private String description;
     //@Column(nullable = true)
     private int owner_id;
     private String item_image_url;
+    @NotBlank(message= "Initial price is required.")
     private int initial_price;
-    private String start_date;
-    private Date stop_date;
+    // We can set this with the current time when creating an item.
+    private Timestamp start_date;
+//    @NotNull(message= "Ending time is required.")
+//    @NotBlank(message= "Ending time is required.")
+    private Timestamp stop_date;
     /*@OneToOne
     @JoinColumn(name = "owner_id", updatable=false, insertable = false)
     private User owner;*/
