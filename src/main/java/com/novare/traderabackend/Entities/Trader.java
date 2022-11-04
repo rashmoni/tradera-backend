133f09687c5156ee2715cc656e5e10c1fe59e9c2
Malla <mallagronqvist@gmail.com>
2022-11-04 15:45:52 +0100

    Added constraint validation. Refactored User to Trader.

--- src/main/java/com/novare/traderabackend/Entities/Trader.java
+++ src/main/java/com/novare/traderabackend/Entities/Trader.java
@@ -6,23 +6,37 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 
 @Entity
-@Table(name ="users")
+@Table(name ="traders")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class User {
+public class Trader {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
+
+    @NotNull(message = "Name is required.")
+    @NotBlank(message = "Name is required.")
+    @Size(min = 3, max = 50, message = "Name must be between 3 and 50 characters")
     private String name;
+    @NotBlank @NotNull @Email(message = "Invalid email address.")
     private String email;
+    @NotBlank(message = "Password is required.")
+    @NotNull(message = "Password is required.")
+    @Size(min = 8, max = 50, message = "Password must be between 8 and 50 characters")
     private String password;
 
-    public User(String name, String email, String password) {
+    public Trader(String name, String email, String password) {
         this.name = name;
         this.email = email;
         this.password = password;
