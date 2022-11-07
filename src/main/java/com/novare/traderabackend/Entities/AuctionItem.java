6806fc610ca6a7f124408cf84a2981e778e434aa
Malla <mallagronqvist@gmail.com>
2022-11-07 10:57:38 +0100

    New branch.

--- src/main/java/com/novare/traderabackend/Entities/AuctionItem.java
+++ src/main/java/com/novare/traderabackend/Entities/AuctionItem.java
@@ -4,9 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import javax.persistence.*;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import java.sql.Timestamp;
+//import javax.validation.constraints.NotBlank;
+//import javax.validation.constraints.NotNull;
+//import java.sql.Timestamp;
 import java.util.Date;
 
 @Entity
@@ -19,20 +19,20 @@ public class AuctionItem {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
-    @NotBlank(message = "Name is required.")
+//    @NotBlank(message = "Name is required.")
     private String item_name;
-    @NotBlank(message= "Description is required.")
+//    @NotBlank(message= "Description is required.")
     private String description;
     //@Column(nullable = true)
     private int owner_id;
     private String item_image_url;
-    @NotBlank(message= "Initial price is required.")
+//    @NotBlank(message= "Initial price is required.")
     private int initial_price;
     // We can set this with the current time when creating an item.
-    private Timestamp start_date;
+    private Date start_date;
 //    @NotNull(message= "Ending time is required.")
 //    @NotBlank(message= "Ending time is required.")
-    private Timestamp stop_date;
+    private Date stop_date;
     /*@OneToOne
     @JoinColumn(name = "owner_id", updatable=false, insertable = false)
     private User owner;*/
