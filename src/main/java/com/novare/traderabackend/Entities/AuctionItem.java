25079efffcd5e512170866598b5453a6c4f1fd69
Malla <mallagronqvist@gmail.com>
2022-11-07 11:02:11 +0100

    Trying to rebase.

--- src/main/java/com/novare/traderabackend/Entities/AuctionItem.java
+++ src/main/java/com/novare/traderabackend/Entities/AuctionItem.java
@@ -19,20 +19,28 @@ public class AuctionItem {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
+
 //    @NotBlank(message = "Name is required.")
     private String item_name;
+
 //    @NotBlank(message= "Description is required.")
     private String description;
+
     //@Column(nullable = true)
     private int owner_id;
+
     private String item_image_url;
+
 //    @NotBlank(message= "Initial price is required.")
     private int initial_price;
+
     // We can set this with the current time when creating an item.
     private Date start_date;
+
 //    @NotNull(message= "Ending time is required.")
 //    @NotBlank(message= "Ending time is required.")
     private Date stop_date;
+
     /*@OneToOne
     @JoinColumn(name = "owner_id", updatable=false, insertable = false)
     private User owner;*/
--- src/main/java/com/novare/traderabackend/Entities/Trader.java
+++ src/main/java/com/novare/traderabackend/Entities/Trader.java
@@ -8,10 +8,7 @@ import lombok.NoArgsConstructor;
 
 
 import javax.persistence.*;
-//import javax.validation.constraints.Email;
-//import javax.validation.constraints.NotBlank;
-//import javax.validation.constraints.NotNull;
-//import javax.validation.constraints.Size;
+
 
 
 @Entity
@@ -25,15 +22,11 @@ public class Trader {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
 
-//    @NotNull(message = "Name is required.")
-//    @NotBlank(message = "Name is required.")
-//    @Size(min = 3, max = 50, message = "Name must be between 3 and 50 characters")
+
     private String name;
-//    @NotBlank @NotNull @Email(message = "Invalid email address.")
+
     private String email;
-//    @NotBlank(message = "Password is required.")
-//    @NotNull(message = "Password is required.")
-//    @Size(min = 8, max = 50, message = "Password must be between 8 and 50 characters")
+
     private String password;
 
     public Trader(String name, String email, String password) {
