bd153d422bf3d119c96f96709378e8564d1b8687
rashmoni <rashudey9851@gmail.com>
2022-11-15 18:14:07 +0100

    Refactor the AuctionController class

--- src/main/java/com/novare/traderabackend/Controller/AuctionController.java
+++ src/main/java/com/novare/traderabackend/Controller/AuctionController.java
@@ -1,17 +1,10 @@
 package com.novare.traderabackend.Controller;
 
 import com.novare.traderabackend.Entities.AuctionItem;
-import com.novare.traderabackend.Repository.AuctionRepo;
 import com.novare.traderabackend.Service.AuctionService;
-import com.novare.traderabackend.Service.FileSystemStorageService;
-import com.novare.traderabackend.Service.IStorageService;
-import com.novare.traderabackend.Utils.FileUtil;
 import lombok.extern.log4j.Log4j2;
-import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import java.sql.Date;
-import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -23,17 +16,6 @@ public class AuctionController {
 
     @Autowired
     AuctionService auctionService;
-    @Autowired
-    AuctionRepo auctionRepo;
-
-    @Autowired
-    FileSystemStorageService fileSystemStorageService;
-
-    private final IStorageService iStorageService;
-
-    public AuctionController(IStorageService iStorageService) {
-        this.iStorageService = iStorageService;
-    }
 
     @GetMapping("/auctions")
     public List<AuctionItem> getAllItems() {
@@ -45,33 +27,10 @@ public class AuctionController {
 
     @PostMapping("/auctions/create")
     public String postNewAuction(@RequestBody AuctionItem auctionItem) {
-        String message = "User tried to access /auctions/create endpoint: " +  auctionItem.getItem_name() ;
+        String message = "User tried to create new item witth name: " +  auctionItem.getItem_name() ;
         log.info(message);
-
-        if(auctionItem.getItem_image_url() != null) {
-
-            message = "trying to convert base64 image and store it to filesystem";
-            log.info(message);
-
-            String imageDataBytes = FileUtil.getImageFromBase64(auctionItem.getItem_image_url());
-            byte [] decodedBytes = Base64.decodeBase64(imageDataBytes);
-            String imageURL = this.fileSystemStorageService.storeBase64(decodedBytes);
-            String baseURL = "http://localhost:9000/files/";
-            String completeURL = baseURL + imageURL;
-
-            message = "image successfully stored, image url is: "+ completeURL;
-            log.info(message);
-
-            auctionItem.setItem_image_url(completeURL);
-        }
-
-        Date today = Date.valueOf(LocalDate.now());
-        auctionItem.setStart_date(today);
 
         auctionService.postNewAuction(auctionItem);
-        message = "AuctionItem is save in database";
-        log.info(message);
-
         return (auctionItem.getItem_name() + " is successfully created");
     }
 
@@ -82,7 +41,7 @@ public class AuctionController {
         String message = "User tried to access /auctions/id endpoint with id: "+id;
         log.info(message);
 
-        return auctionRepo.findById(id);
+        return auctionService.getOneItem(id);
     }
 
     @GetMapping("/auctions/search/{name}")
--- src/main/java/com/novare/traderabackend/Service/AuctionService.java
+++ src/main/java/com/novare/traderabackend/Service/AuctionService.java
@@ -2,26 +2,69 @@ package com.novare.traderabackend.Service;
 
 import com.novare.traderabackend.Entities.AuctionItem;
 import com.novare.traderabackend.Repository.AuctionRepo;
+import com.novare.traderabackend.Utils.FileUtil;
+import lombok.extern.log4j.Log4j2;
+import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import java.sql.Date;
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 @Service
+@Log4j2
 public class AuctionService {
     @Autowired
     AuctionRepo auctionRepo;
+
+    @Autowired
+    FileSystemStorageService fileSystemStorageService;
+
+    private final IStorageService iStorageService;
+
+    public AuctionService(IStorageService iStorageService) {
+        this.iStorageService = iStorageService;
+    }
+
     public List<AuctionItem> getAllItems() {
         List<AuctionItem> allAuctionsItem =  auctionRepo.findAll();
         return allAuctionsItem;
     }
 
     public void postNewAuction(AuctionItem auctionItem) {
-        //write the logic to check if the item is already present in DB
-        AuctionItem savedAuction = auctionRepo.save(auctionItem);
+        String message;
+
+        if(auctionItem.getItem_image_url() != null) {
+
+            message = "trying to convert base64 image and store it to filesystem";
+            log.info(message);
+            String imageDataBytes = FileUtil.getImageFromBase64(auctionItem.getItem_image_url());
+            byte [] decodedBytes = Base64.decodeBase64(imageDataBytes);
+            String imageURL = this.fileSystemStorageService.storeBase64(decodedBytes);
+            String baseURL = "http://localhost:9000/files/";
+            String completeURL = baseURL + imageURL;
+
+            message = "image successfully stored, image url is: "+ completeURL;
+            log.info(message);
+
+            auctionItem.setItem_image_url(completeURL);
+        }
+
+        Date today = Date.valueOf(LocalDate.now());
+        auctionItem.setStart_date(today);
+
+        auctionRepo.save(auctionItem);
+        message = "AuctionItem is save in database";
+        log.info(message);
     }
 
     public List<AuctionItem> getByItemName(String name) {
         return auctionRepo.findAuctionByName(name);
 
+    }
+
+    public Optional<AuctionItem> getOneItem(Long id) {
+        return auctionRepo.findById(id);
     }
 }
